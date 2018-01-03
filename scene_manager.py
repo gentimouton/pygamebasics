975882de24eec933aeb11af4fdd405295f485625
gentimouton <gentimouton@gmail.com>
2018-01-02 21:38:40 -0800

    Update scene_manager.py

--- scene_manager.py
+++ scene_manager.py
@@ -1,98 +1,91 @@
-import pygame
-from pygame.constants import QUIT, KEYDOWN, K_ESCAPE, K_RETURN, K_F4
+import pygame as pg
 
 
 # constants.py
-CMD_SUBMIT = 'submit'
-CMD_CANCEL = 'cancel'
-
+BTN_SUBMIT = 'submit'
+BTN_CANCEL = 'cancel'
 
 
 # scenes.py
 SCN_WORLD = 'world scene'
 SCN_MENU = 'menu scene'
+SCN_QUIT = 'quit scene'  # not a real scene, but a convenient value to quit
+
 class Scene():
-    def __init__(self, scene_manager, screen):
-        self._scene_manager = scene_manager
-        self._screen = screen
-    def reset(self, params):
+    def resume(self, **kwargs):
+        """ Called by scene manager. kwargs are passed by previous scene """ 
         pass
-    def process_input(self):
-        pass
-    def tick(self):
-        pass
-    def render(self):
-        pass
+    def tick(self, buttons):
+        """ Based on buttons being pressed, run scene logic and render it. 
+        Return next_scene_id, kwargs for next scene, if any. """
+        return None, {}
+    
         
 class MainMenuScene(Scene):
-    def __init__(self, scene_manager, screen):
-        Scene.__init__(self, scene_manager, screen)
-        self.frame_num = 0
-        self.font = pygame.font.Font(None, 30)
-    def process_input(self, cmd):
-        if cmd == CMD_SUBMIT:
-            params = {'counter': self.frame_num}
-            self._scene_manager.change_scene(SCN_WORLD, params)
-        elif cmd == CMD_CANCEL:
-            self._scene_manager.game_over = True
-    def tick(self):
-        self.frame_num += 1
-    def render(self):
-        self._screen.fill((0, 0, 111))
-        txt_surf = self.font.render(str(self.frame_num), 1, (255, 255, 255))
-        self._screen.blit(txt_surf, (200, 200))
+    def __init__(self):
+        self.ticks = 0
         
-class WorldScene(Scene):
-    def reset(self, params):
-        self.font = pygame.font.Font(None, 30)
-        txt = str(params['counter']) if 'counter' in params else '???'
-        self.txt_surf = self.font.render(str(txt), 1, (0, 0, 0))
-    def process_input(self, cmd):
-        if cmd == CMD_CANCEL:
-            self._scene_manager.change_scene(SCN_MENU)
-    def render(self):
-        self._screen.fill((0, 255, 0))
-        self._screen.blit(self.txt_surf, (300, 300))
+    def tick(self, buttons):
+        if buttons[BTN_SUBMIT]:
+            return SCN_WORLD, {'counter': self.ticks}
+        elif buttons[BTN_CANCEL]:
+            return SCN_QUIT, {}
+        self.ticks += 1
+        pg.display.get_surface().fill((55, 55, 111))
+        blit_txt('Menu- %d ticks.' % self.ticks, (111, 222, 55), (50, 200))
+        blit_txt('[Enter]: world, [ESC]: quit', (111, 222, 55), (50, 300))
+        pg.display.flip()
+        return None, {}
 
+    
+class WorldScene(Scene):
+    def resume(self, **kwargs):
+        self.counter = kwargs.get('counter', -1)  # -1 if counter not passed
+            
+    def tick(self, buttons):
+        if buttons[BTN_CANCEL]:
+            return SCN_MENU, {}
+        pg.display.get_surface().fill((55, 111, 222))
+        blit_txt('World- received %d.' % self.counter, (111, 55, 55), (50, 100))
+        blit_txt('[ESC]: menu', (111, 55, 55), (50, 150))
+        pg.display.flip()
+        return None, {}
+
+
+def blit_txt(txt, color, pos):  # blit text onto screen
+    surf = pg.font.Font(None, 30).render(txt, 1, color)
+    pg.display.get_surface().blit(surf, pos)
+    
 
 # main.py
-pygame.init()
-
-        
-class SceneManager():  # main file: state machine
-    def __init__(self):
-        self.game_over = False
-        self.clock = pygame.time.Clock()
-        self._screen = pygame.display.set_mode((500, 500))
-        main_menu_scene = MainMenuScene(self, self._screen)
-        world_scene = WorldScene(self, self._screen)
-        self.scenes = {SCN_MENU: main_menu_scene, SCN_WORLD: world_scene}
-        self.cur_scene = self.scenes[SCN_MENU]
-        
-    def run(self):
-        while not self.game_over:
-            self.clock.tick(60)
-            pressed_keys = pygame.key.get_pressed()
-            alt_held = pressed_keys[pygame.K_LALT] or pressed_keys[pygame.K_RALT]
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    self.game_over = True
-                if event.type == KEYDOWN:
-                    key = event.key
-                    if key == K_F4 and alt_held:
-                        self.game_over = True
-                    elif key == K_ESCAPE:
-                        self.cur_scene.process_input(CMD_CANCEL)  # may change cur_scene
-                    elif key == K_RETURN:
-                        self.cur_scene.process_input(CMD_SUBMIT)  # may change cur_scene
-            self.cur_scene.tick()
-            self.cur_scene.render()
-            pygame.display.update()
-    
-    def change_scene(self, new_scene_name, new_scene_params={}):
-        if new_scene_name in self.scenes.keys():
-            self.cur_scene = self.scenes[new_scene_name]
-            self.cur_scene.reset(new_scene_params)
+def main():  # scene manager, state machine
+    pg.init()
+    pg.display.set_mode((500, 500))
+    # setup scenes
+    scenes = { SCN_MENU: MainMenuScene(), SCN_WORLD: WorldScene() }
+    cur_scene = scenes[SCN_MENU]
+    # map keyboard keys to abstract commands understood by scenes
+    key2cmd = { pg.K_ESCAPE: BTN_CANCEL, pg.K_RETURN: BTN_SUBMIT }
+    # loop
+    clock = pg.time.Clock()
+    done = False
+    while not done:
+        clock.tick(10)
+        # find which buttons were pressed this tick
+        buttons_pressed = { BTN_CANCEL: 0, BTN_SUBMIT: 0}
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                done = True
+            if event.type == pg.KEYDOWN and event.key in key2cmd:
+                cmd = key2cmd[event.key]
+                buttons_pressed[cmd] = 1
+        # tick current scene, pass it the state of the buttons
+        next_scene_id, kwargs = cur_scene.tick(buttons_pressed)
+        if next_scene_id == SCN_QUIT:
+            done = True
+        elif next_scene_id:
+            cur_scene = scenes[next_scene_id]
+            cur_scene.resume(**kwargs)
 
 if __name__ == "__main__":
-    SceneManager().run()
+    main()
